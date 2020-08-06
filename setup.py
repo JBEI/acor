4adbef880ccbec6d1a374b57c4b2c64b5d9dbda5
Mark Forrer <mark.forrer@lbl.gov>
2020-08-06 15:05:38 -0700

    Fix broken install for pip install -e
    Should fix install errors by avoiding numpy import until after numpy is installed

--- setup.py
+++ setup.py
@@ -3,32 +3,31 @@
 
 import sys
 import os
+from setuptools import dist
 
 try:
     from setuptools import setup, Extension
-    setup, Extension
 except ImportError:
     from distutils.core import setup, Extension
-    setup, Extension
-
-import numpy
 
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     sys.exit()
 
-
 desc = open("README.rst").read()
-required = ["numpy"]
 
+# install numpy so we can import it before calling setup
+# https://luminousmen.com/post/resolve-cython-and-numpy-dependencies
+dist.Distribution().fetch_build_eggs(['numpy'])
+import numpy
 include_dirs = [
     "acor",
     numpy.get_include(),
 ]
+
 acor = Extension("acor._acor", ["acor/_acor.c", "acor/acor.c"],
                  include_dirs=include_dirs)
-
 
 setup(
     name="acor",
@@ -36,11 +35,11 @@ setup(
     author="Daniel Foreman-Mackey and Jonathan Goodman",
     author_email="danfm@nyu.edu",
     packages=["acor"],
-    url="http://github.com/dfm/acor",
+    url="http://github.com/dfm/chimaerase",
     license="MIT",
     description="Estimate the autocorrelation time of a time series quickly.",
     long_description=desc,
-    install_requires=required,
+    install_requires=["numpy"],
     ext_modules=[acor],
     classifiers=[
         "Development Status :: 3 - Alpha",
